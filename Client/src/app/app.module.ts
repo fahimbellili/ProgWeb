380f193891742cebaa60b43f6444e773f867d9f2
Fahim BELLILI <fahimbellili@gmail.com>
2019-02-16 00:20:07 +0100

    Add routing

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -2,25 +2,34 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 
-
 import {AppComponent} from './app.component';
 import {MaterialModule} from './material.module';
 import {NavbarComponent} from './navbar/navbar.component';
 import {Server} from "../../providers/server";
 import {HttpClientModule} from "@angular/common/http";
 import {ProductViewComponent} from './product-view/product-view.component';
+import {RouterModule, Routes} from "@angular/router";
+import {RecipeViewComponent} from "./recipe-view/recipe-view.component";
+
+const appRoutes: Routes = [
+    {path: 'product', component: ProductViewComponent},
+    {path: 'recipe', component: RecipeViewComponent},
+    {path: '', component: ProductViewComponent}
+];
 
 @NgModule({
     declarations: [
         AppComponent,
         NavbarComponent,
         ProductViewComponent,
+        RecipeViewComponent
     ],
     imports: [
         BrowserModule,
         MaterialModule,
         FormsModule,
-        HttpClientModule
+        HttpClientModule,
+        RouterModule.forRoot(appRoutes),
     ],
     providers: [Server],
     bootstrap: [AppComponent]
--- Client/src/app/recipe-view/recipe-view.component.ts
+++ Client/src/app/recipe-view/recipe-view.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-recipe-view',
-  templateUrl: './recipe-view.component.html',
-  styleUrls: ['./recipe-view.component.css']
+    selector: 'app-recipe-view',
+    templateUrl: './recipe-view.component.html',
+    styleUrls: ['./recipe-view.component.css']
 })
 export class RecipeViewComponent implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }

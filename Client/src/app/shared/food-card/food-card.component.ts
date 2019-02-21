dc9cffc6a0b25327a1ce769ef5813e1fc73ceb85
Tarik HAMLAT <hthamlat@gmail.com>
2019-02-21 22:52:04 +0100

    Implement method getImgUrl todisplay Image

--- Client/src/app/shared/food-card/food-card.component.ts
+++ Client/src/app/shared/food-card/food-card.component.ts
@@ -13,8 +13,12 @@ export class FoodCardComponent implements OnInit {
     modalRef: BsModalRef;
     @Input() food: any;
     resultReq: any;
+    defaultUrl = 'https://static.thenounproject.com/png/220984-200.png';
 
     constructor(private modalService: BsModalService, public server: Server) {
+    }
+
+    ngOnInit() {
     }
 
     openModal() {
@@ -31,7 +35,20 @@ export class FoodCardComponent implements OnInit {
 
     }
 
-    ngOnInit() {
+    getImgUrl() {
+        const o = this.food;
+        let url = this.defaultUrl;
+        if (o.images.hasOwnProperty('front_fr')) {
+            let id;
+            if (o.id.length === 13) {
+                id = o.id.substring(0, 3) + '/' + o.id.substring(3, 6) + '/' + o.id.substring(6, 9) + '/' + o.id.substring(9, 13);
+            } else {
+                id = o.id;
+            }
+            url = 'https://static.openfoodfacts.org/images/products/' + id + '/front_fr.' + o.images['front_fr'].rev + '.200.jpg';
+        }
+        return url;
     }
+
 
 }

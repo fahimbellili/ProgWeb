ecdcbcd856b27ba169c8e88a8e6ab2464270c7c5
Fahim BELLILI <fahimbellili@gmail.com>
2019-02-23 14:37:35 +0100

    Fix images size inside cards

--- Client/src/app/price-view/price-view.component.ts
+++ Client/src/app/price-view/price-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 import * as L from 'leaflet';
 import {Server} from "../../../providers/server";

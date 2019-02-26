4c1677571141d248a628534ada68e7ff58ec7736
Unknown <canorom@hotmail.fr>
2019-02-26 12:10:46 +0100

    try to fix CORS issue

--- Client/providers/server.ts
+++ Client/providers/server.ts
@@ -7,49 +7,55 @@ import {Injectable} from '@angular/core';
 export class Server {
 
     path: string;
+    config = {headers:  {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        }
+    };
 
     constructor(private http: HttpClient) {
         this.path = 'https://progweb2019server.herokuapp.com';
     }
 
     getAllScore(idProduct): Observable<any> {
-        return this.http.get(this.path + '/getScore/' + idProduct);
+        return this.http.get(this.path + '/getScore/' + idProduct, this.config);
     }
 
     getAllProductId(id): Observable<any> {
-        return this.http.get(this.path + '/getProduct/' + id);
+        return this.http.get(this.path + '/getProduct/' + id, this.config);
     }
 
     getComments(id): Observable<any> {
-        return this.http.get(this.path + '/getComments/' + id);
+        return this.http.get(this.path + '/getComments/' + id, this.config);
     }
 
     getRecipe(id): Observable<any> {
-        return this.http.get(this.path + '/getRecipe/' + id);
+        return this.http.get(this.path + '/getRecipe/' + id, this.config);
     }
 
     getAllProducts(): Observable<any> {
-        return this.http.get(this.path + '/getAll');
+        return this.http.get(this.path + '/getAll', this.config);
     }
 
     getAllAlimentsBio(): Observable<any> {
-        return this.http.get(this.path + '/getAlimentsBio');
+        return this.http.get(this.path + '/getAlimentsBio', this.config);
     }
 
     getAllAlimentsWithoutAllergens(): Observable<any> {
-        return this.http.get(this.path + '/getAlimentsWithoutAllergens');
+        return this.http.get(this.path + '/getAlimentsWithoutAllergens', this.config);
     }
 
     getAllRecipes(): Observable<any> {
-        return this.http.get(this.path + '/getAllRecipes');
+        return this.http.get(this.path + '/getAllRecipes', this.config);
     }
 
     getRecipesBio(): Observable<any> {
-        return this.http.get(this.path + '/getRecipeBio');
+        return this.http.get(this.path + '/getRecipeBio', this.config);
     }
 
     getAllPrices(): Observable<any> {
-        return this.http.get(this.path + '/getAllPrices');
+        return this.http.get(this.path + '/getAllPrices', this.config);
     }
 
 }
